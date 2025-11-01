74bd877bf227d8db14dfc3a0eaa5d922fb587a14
Srinath Reddy <srinathreddy@Srinaths-MacBook-Air.local>
2025-11-02 03:34:08 +0530

    added passowrd strength

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -2,8 +2,16 @@ import Joi from 'joi';
 
 export const registerSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().min(8).max(128).required().pattern(/[0-9]/).pattern(/[A-Z]/),
-  roles: Joi.array().items(Joi.string()).optional(),
+  password: Joi.string().min(8).max(128).pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+      'password strength'
+    )
+    .required()
+    .messages({
+      'string.pattern.name':
+        'Password must include at least one uppercase letter, one lowercase letter, one number, and one special character.',
+      'string.min': 'Password must be at least 8 characters long.',
+    })
 });
 
 export const loginSchema = Joi.object({
